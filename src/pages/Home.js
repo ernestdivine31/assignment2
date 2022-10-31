ceda9cb1f6e402903d9e4164f8b29db9e1f74ac3
ernestdivine31 <102002371+ernestdivine31@users.noreply.github.com>
2022-10-31 01:37:27 +0100

    modified images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import home from "../assets/images/image-home.jpg";
+import home2 from "../assets/images/home2.jpg";
+import home3 from "../assets/images/home3.jpg";
+import home4 from "../assets/images/home4.jpg";
 import NavbarMobile from "../components/NavbarMobile";
 
 const Home = () => {
@@ -15,8 +18,11 @@ const Home = () => {
             IF CHILDREN DRAWINGS WERE LIVING CREATURES
            </b>
           </p>
-          <div className="home-img">
-            <img src={home} alt="home" />
+          <div className="home-img"> 
+          <img id="home" src={home} alt="home" /> 
+          <img id="home2" src={home2} alt="home" />
+          <img id="home3" src={home3} alt="home" />
+          <img id="home4" src={home4} alt="home" />
           </div>
         </div>
       </div>
